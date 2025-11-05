fe0747bcae8a44415502a54ad4938804627eae62
sirigirikirank2019-png <sirigirikirank2019@gmail.com>
2025-11-05 20:36:09 +0530

    Update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -1,55 +1,10 @@
-pipeline {
-    agent any
+#!/bin/bash
+JMETER_HOME = 'C:\\Users\\sreek\\OneDrive\\Desktop\\Softwares\\apache-jmeter-5.6.3\\apache-jmeter-5.6.3'
+TEST_PLAN=tests/SimplePipelineTest.jmx
+RESULTS_DIR=results
+INFLUXDB_URL=http://localhost:8086/write?db=jmeter
 
-    environment {
-        // Path to your Java JDK
-        JAVA_HOME = 'C:\\Program Files\\Java\\jdk-17'
-        // Path to JMeter
-        JMETER_HOME = 'C:\\Users\\sreek\\OneDrive\\Desktop\\Softwares\\apache-jmeter-5.6.3\\apache-jmeter-5.6.3'
-        // Prepend Java and JMeter to PATH
-        PATH = "${env.JAVA_HOME}\\bin;${env.JMETER_HOME}\\bin;${env.PATH}"
-    }
-
-    stages {
-        stage('Check Java') {
-            steps {
-                bat '''
-                echo ===== Java Check =====
-                java -version
-                echo JAVA_HOME=%JAVA_HOME%
-                '''
-            }
-        }
-
-        stage('Prepare Results Folder') {
-            steps {
-                bat '''
-                echo ===== Preparing Results Folder =====
-                if not exist results mkdir results
-                '''
-            }
-        }
-
-        stage('Run JMeter Test') {
-            steps {
-                bat '''
-                echo ===== Running JMeter =====
-                cd "%WORKSPACE%"
-                jmeter.bat -n -t tests/SimplePipelineTest.jmx -l results/simpleresult.jtl
-                '''
-            }
-        }
-
-        stage('Post-Build') {
-            steps {
-                echo '=== JMeter Test Completed ==='
-            }
-        }
-    }
-
-    post {
-        always {
-            echo 'Pipeline finished.'
-        }
-    }
-}
+mkdir -p $RESULTS_DIR
+$JMETER_HOME/bin/jmeter -n -t $TEST_PLAN -l $RESULTS_DIR/result.jtl \
+  -e -o $RESULTS_DIR/report \
+  -JinfluxdbUrl=$INFLUXDB_URL
